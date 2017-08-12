f774b30f904a280bf0dc3ef7627502cfbc407d19
jithin971 <jithin971@gmail.com>
2017-08-12 13:23:55 +0530

    added routing

--- angdemo/src/app/app.module.ts
+++ angdemo/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import{Routes,RouterModule }from '@angular/router';
 
+
+const route:Routes=[
+  {path:'about',component:HomeComponent},
+   {path:'home',component:HomeComponent}
+  ]
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(this.route)
   ],
   providers: [],
   bootstrap: [AppComponent]
